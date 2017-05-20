bf0131726c721c2a5b34229086bbd1ffff3d9948
dydokamil <dydokamil@gmail.com>
2017-05-20 12:38:00 +0200

    Load images

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from tools import TRAIN_PATH, create_paths, load_labels, LABELS_PATH
+from tools import TRAIN_PATH, create_paths, load_labels, LABELS_PATH, load_images
+from PIL import Image
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # print(create_paths(TRAIN_PATH))
-    invasive = load_labels(LABELS_PATH)
+    # invasive = load_labels(LABELS_PATH)
     # print(id)
-    print(invasive)
+    # print(invasive)
+    image_paths = create_paths(TRAIN_PATH)
+    images = load_images(image_paths, size=20)
+    plt.imshow(images[0])
+    plt.show()
--- tools.py
+++ tools.py
@@ -1,6 +1,8 @@
 import os
+import random
 import pandas as pd
 import numpy as np
+from PIL import Image
 
 PATH = '/media/hdd/training_data/invasive-species/'
 TRAIN_PATH = os.path.join(PATH, 'train')
@@ -17,3 +19,14 @@ def create_paths(path):
 def load_labels(path):
     labels = pd.read_csv(path)
     return dict(zip(np.array(labels['name']), np.array(labels['invasive'])))
+
+
+def load_images(file_paths, size=0):
+    images = []
+
+    if size == 0:
+        size = len(file_paths)
+    mask = random.sample(range(len(file_paths)), k=size)
+    for i in mask:
+        images.append(np.array(Image.open(file_paths[i])))
+    return images
