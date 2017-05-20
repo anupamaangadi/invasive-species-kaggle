4e4fb1c6a4fb4c63eb3172edcae1b299657ceb6d
dydokamil <dydokamil@gmail.com>
2017-05-20 12:56:26 +0200

    Added grayscale, normalization

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from tools import TRAIN_PATH, create_paths, load_labels, LABELS_PATH, load_images
+import cv2
+
+from tools import TRAIN_PATH, create_paths, load_labels, LABELS_PATH, load_images, normalize
 from PIL import Image
 import matplotlib.pyplot as plt
 
@@ -8,6 +10,8 @@ if __name__ == '__main__':
     # print(id)
     # print(invasive)
     image_paths = create_paths(TRAIN_PATH)
-    images = load_images(image_paths, size=20)
-    plt.imshow(images[0])
-    plt.show()
+    images = load_images(image_paths, grayscale=True, size=20)
+    # normalize(images)
+    cv2.imshow('gray_image', images[0])
+    cv2.waitKey(0)
+    # plt.show()
--- tools.py
+++ tools.py
@@ -1,5 +1,7 @@
 import os
 import random
+
+import cv2
 import pandas as pd
 import numpy as np
 from PIL import Image
@@ -21,12 +23,23 @@ def load_labels(path):
     return dict(zip(np.array(labels['name']), np.array(labels['invasive'])))
 
 
-def load_images(file_paths, size=0):
+def load_images(file_paths, grayscale=True, size=0):
     images = []
 
     if size == 0:
         size = len(file_paths)
     mask = random.sample(range(len(file_paths)), k=size)
     for i in mask:
-        images.append(np.array(Image.open(file_paths[i])))
+        images.append(np.array(cv2.imread(file_paths[i])))
+
+    if grayscale:
+        return [cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) for image in images]
     return images
+
+
+def normalize(images):
+    return np.array([image / 255. for image in images])
+
+
+def resize(images, size=(224, 224, 3)):
+    pass
